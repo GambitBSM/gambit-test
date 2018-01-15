1437560bfa0f81147b753d2460d8915283ad7983
andrew <andrew.j.fowlie@googlemail.com>
2018-01-16 10:55:47 +1100

    Echo info file to help debugging

--- gambit.bash
+++ gambit.bash
@@ -13,13 +13,25 @@ gambit_id_ascii_files() {
   local rtol=$4
   local atol=$5
   
-  # Safely delete an existing gambit result
+  # Safely delete an existing GAMBIT result file
 
   if [ -n "$gambit_result" ] && [ -f "$gambit_result" ] && [ -n "$(find ./data_gambit -wholename "$gambit_result")" ]; then
     rm -f "$gambit_result"
   fi
+  
+  # Call GAMBIT
         
   "$GAMBIT"/gambit -rf "$yaml"
+  
+  # Print information about GAMBIT result file
+  
+  local gambit_info="$gambit_result"_info
+  
+  if [ -f "$gambit_info" ]; then
+    cat "$gambit_info"
+  fi
+  
+  # Check GAMBIT result file against expected file
   
   id_ascii_files "$gambit_result" "$gambit_expected" "$rtol" "$atol"
 }
